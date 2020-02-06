437b8e581536d35d03cfcf61251d06abf77c73dc
holbertontaekim1107 <1701@holbertonschool.com>
2020-02-06 05:00:48 +0000

    Formatted c program properly

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
-
+d
+/**
+* main - Entry point
+*
+* Return: Always 0
+*
+*/
 int main(void)
 {
-  printf("with proper grammar, but the outcome is a piece of art, \n");
-  return (0);
+	printf("with proper grammar, but the outcome is a piece of art, \n");
+	return (0);
 }
