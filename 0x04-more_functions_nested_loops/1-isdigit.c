ab48123d9404961ca2b831a823bd8fa12c6b0b1c
holbertontaekim1107 <1701@holbertonschool.com>
2020-02-14 05:34:50 +0000

    fixed format so betty is happy

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,11 +1,11 @@
 #include "holberton.h"
 
 /**
-* _isdigit - checks if input in ASCII is digit between 0-9. 
+* _isdigit - checks if input in ASCII is digit between 0-9.
 * @c: input variable to check ASCII value
 * Return -1 if digit, 0 if not a digit.
 */
 int _isdigit(int c)
 {
-	return(c >= '0' && c <= '9' ? 1 : 0);
+	return (c >= 47 && c <= 58 ? 1 : 0);
 }
