98d104eb1c24ecfd078ba148cfa31aa42ed7c317
holbertontaekim1107 <1701@holbertonschool.com>
2020-02-14 05:28:45 +0000

    spelling error fixed for main directory

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,11 @@
+#include "holberton.h"
+
+/**
+* int mul - multiply the two integer.
+*
+* Return - value of two integers multiplied.
+*/
+int mul(int a, int b)
+{
+	return (a * b);
+}
---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+
+/**
+* print_numbers - print number from 0-9.
+*
+* Return - void
+*/
+void print_numbers(void)
+{
+	char a;
+
+	for (a = 0; a <= 9; a++)
+
+	{
+		_putchar(a);
+	}
+	_puchar('\n');
+}
