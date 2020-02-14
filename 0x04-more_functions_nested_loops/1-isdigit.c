8c0f7a1d36183bc9d443b51f1bdb511c263b18e9
holbertontaekim1107 <1701@holbertonschool.com>
2020-02-14 05:38:13 +0000

    new code created, checker unhappy with certain things

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,12 +1,20 @@
 #include "holberton.h"
 
 /**
-* _isdigit - checks if input in ASCII is digit between 0-9.
-* @c: input variable to check ASCII value
-*
-* Return -1 if digit, 0 if not a digit.
-*/
+ * _isdigit - checks if char is a
+ * digit, else, prints 0
+ * @c: variable imported from main.c
+ * Return: 1 if capital letter, else, 0
+ */
+
 int _isdigit(int c)
 {
-	return (c >= 47 && c <= 58 ? 1 : 0);
+	if (c >= '0' && c <= '9')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
 }
