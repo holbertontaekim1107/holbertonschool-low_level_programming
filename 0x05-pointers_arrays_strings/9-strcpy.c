95beac287bc02170f54f4d576ed0d3263db1fbc2
holbertontaekim1107 <1701@holbertonschool.com>
2020-02-18 23:52:03 +0000

    function that copies the string pointed to by src

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+
+/**
+ * _strcpy - prints one char out of 2 of a string
+ *
+ * @dest: string value
+ * @src: string pointer
+ *
+ * Return: dest
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int counter = 0, a = 0;
+
+	while (src[counter] != '\0')
+	{
+		counter++;
+	}
+	for (; a < counter; a++)
+	{
+		dest[a] = src[a];
+	}
+	return (dest);
+}
