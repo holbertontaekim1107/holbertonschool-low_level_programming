2c67e55d24ef8199d21ca6137cf1d6fa5972956d
holbertontaekim1107 <1701@holbertonschool.com>
2020-02-25 03:23:33 +0000

    function that fills memory with constant byte

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+/**
+ * _memset - takes value from pointer s and replaces with values from b
+ * @b: var that needs to be replaced
+ * @n: number of bytes/characters to be reassigned
+ * @s: pointer contains value stored in s.
+ * Return: Returns new value to pointer s
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	char *o = s;
+
+	while ( n > 0)
+	{
+		*s = b;
+		s++;
+		n--;
+	}
+return (o);
+}
