974aeba0d7e3ea857fa9be8483862c86ac983b5f
holbertontaekim1107 <1701@holbertonschool.com>
2020-03-02 03:07:42 +0000

    Program that prints number of arg passed into it

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "holberton.h"
+
+/**
+ * main - contains logic for function
+ * @argc: argument count
+ * @argv: disabled
+ * Return: Always 0
+ */
+
+int main(int argc, char *argv[] __attribute__((unused)))
+{
+	printf("%d\n", argc - 1);
+
+return (0);
+}
