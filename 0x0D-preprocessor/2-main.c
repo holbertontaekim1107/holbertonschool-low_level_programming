aac6992b824051eeb25078f5de34c82079263c19
holbertontaekim1107 <1701@holbertonschool.com>
2020-03-10 03:21:10 +0000

    Program to print name of filee it was compiled from

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - simply prints the file
+ * name that was used to compile
+ * this source code
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
