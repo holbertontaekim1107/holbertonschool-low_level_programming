a2abfd8d78603e3b4c7d0569243c762f0b0b582f
holbertontaekim1107 <1701@holbertonschool.com>
2020-03-10 03:14:02 +0000

    function that frees dog

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,17 @@
+#include "dog.h"
+#include <stdlib.h>
+/**
+ * free_dog - frees dog struct
+ * @d: input struct
+ */
+void free_dog(dog_t *d)
+{
+	if (d != NULL)
+	{
+		if (d->name != NULL)
+			free(d->name);
+		if (d->owner != NULL)
+			free(d->owner);
+		free(d);
+	}
+}
