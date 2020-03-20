3deb0cd8ef3cabedb3be81c345a79d05f81cfe94
holbertontaekim1107 <1701@holbertonschool.com>
2020-03-20 04:26:36 +0000

    Function that retursn the num of elements that is linked

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * list_len - counts number of nodes and returns total count of nodes
+ * @h: -> pointer to the nodes
+ * Return: count
+ */
+
+size_t list_len(const list_t *h)
+{
+	size_t count = 0;
+
+	while (h != NULL)
+	{
+		count++;
+		h = h->next;
+	}
+return(count);
+}
