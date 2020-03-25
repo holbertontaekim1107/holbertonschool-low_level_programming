02f273845f8be7622452653c7e5cc10e5e2ba220
holbertontaekim1107 <1701@holbertonschool.com>
2020-03-25 02:37:47 +0000

    Function that returns nth node of list

---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * get_nodeint_at_index - fetches and returns nth node.
+ * @head: - starting olist
+ * @index: - limiter
+ * Return: All nodes upto the limiter
+ */
+
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	unsigned int node;
+
+	for (node = 0; node < index; node++)
+	{
+		if (head == NULL)
+			return (NULL);
+
+		head = head->next;
+	}
+
+	return (head);
+}
