4b7af20d2138f8491baab9ecfff730148a6c7e1a
holbertontaekim1107 <1701@holbertonschool.com>
2020-04-01 04:12:09 +0000

    Function to create file with given conditions

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,32 @@
+#include "holberton.h"
+
+/**
+ * create_file = create a file
+ * @filename: Pointer to name of file to create
+ * @text_content: Pointer to string to write the file
+ * Return: Fails: -1 otherwise: 1
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	int o, w, len = 0;
+
+	if (filename == NULL)
+		return (-1);
+
+	if (text_content != NULL)
+	{
+		for (len = 0; text_content[len];)
+			len++;
+	}
+
+	o = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	w = write(o, text_content, len);
+
+	if (o == -1 || w == -1)
+		return (-1);
+
+	close(o);
+
+	return (1);
+}
