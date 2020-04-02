916ca5b28bea57cbf134973ca1b68ac704f49e14
holbertontaekim1107 <1701@holbertonschool.com>
2020-04-02 02:38:00 +0000

    added adv task prototype to header file

--- 0x06-pointers_arrays_strings/holberton.h
+++ 0x06-pointers_arrays_strings/holberton.h
@@ -2,18 +2,13 @@
 #define HOLBERTON_H
 
 char *_strcat(char *dest, char *src);
-
+void print_number(int n);
 int _strlen(char *s);
-
 char *_strncat(char *dest, char *src, int n);
-
 char *_strncpy(char *dest, char *src, int n);
-
 void reverse_array(int *a, int n);
-
 char *string_toupper(char *a);
-
 char *cap_string(char *s);
-
 char *leet(char *l);
+
 #endif
